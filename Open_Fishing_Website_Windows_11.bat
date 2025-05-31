783a775ae6e13bf10afcf40757e77c362cdd325f
MyNewAccount-website <tigerlesenko@gmail.com>
2025-05-31 14:52:34 +0200

    Delete Open_Fishing_Website_Windows_11.bat

--- Open_Fishing_Website_Windows_11.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-start "" chrome.exe --kiosk --app="https://mynewaccount-website.github.io/windows_11/" --disable-popup-blocking --disable-web-security --user-data-dir=C:\temp\chrome_test
-
-exit
